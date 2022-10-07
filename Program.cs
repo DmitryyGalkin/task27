ad333a911eb12ae0148edcde326f472dd09cc603
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-10-08 00:01:07 +0300

    task27

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+Ограничения:
+- Должна быть отдельная функция, которая возвразает результат суммы цифр
+- Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода */
+
+Console.Clear();
+
+Console.WriteLine("введите число: ");
+
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0;
+
+SumDigit(num);
+int SumDigit(int number)
+{
+    for (int i = 1; num > 0; i++)
+    {
+        int div = num % 10;
+        sum = sum + div;
+        num = num / 10;
+    }
+    return sum;
+}
+Console.WriteLine(sum);
